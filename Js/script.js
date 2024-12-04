dd023cc0654abdf4dc068724fa2ce3ad6dc73c78
AliShazil <alishazil.dev@gmail.com>
2024-12-04 21:27:29 +0500

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -27,40 +27,40 @@ function formatSeconds(seconds) {
 
 
 
-async function getSongs(folder) {
-    currFolder = folder;
-    let a = await fetch(`/${currFolder}/`)
-    let response = await a.text();
-    // console.log(response);
-    let div = document.createElement("div")
-    div.innerHTML = response
-    let as = div.getElementsByTagName("a")
-    // console.log(as);
-    songs = []
+// async function getSongs(folder) {
+//     currFolder = folder;
+//     let a = await fetch(`/${currFolder}/`)
+//     let response = await a.text();
+//     // console.log(response);
+//     let div = document.createElement("div")
+//     div.innerHTML = response
+//     let as = div.getElementsByTagName("a")
+//     // console.log(as);
+//     songs = []
 
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/${folder}/`)[1])
-        }
-    }
+//     for (let index = 0; index < as.length; index++) {
+//         const element = as[index];
+//         if (element.href.endsWith(".mp3")) {
+//             songs.push(element.href.split(`/${folder}/`)[1])
+//         }
+//     }
 
-    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-    songUL.innerHTML = "";
-    for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li>
-                            <img class="invert" src="img/music.svg" alt="">
-                            <div class="info">
-                                <div>${song.replaceAll("%20", " ")}</div>
-                                <div>Alan Walker</div>
-                            </div>
-                            <div class="playnow">
-                                <span>Play Now</span>
-                                <img src="img/play.svg" alt="">
-                            </div>
-                        </li>
-                    </ul>`;
-    }
+//     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+//     songUL.innerHTML = "";
+//     for (const song of songs) {
+//         songUL.innerHTML = songUL.innerHTML + `<li>
+//                             <img class="invert" src="img/music.svg" alt="">
+//                             <div class="info">
+//                                 <div>${song.replaceAll("%20", " ")}</div>
+//                                 <div>Alan Walker</div>
+//                             </div>
+//                             <div class="playnow">
+//                                 <span>Play Now</span>
+//                                 <img src="img/play.svg" alt="">
+//                             </div>
+//                         </li>
+//                     </ul>`;
+//     }
 
     //Attach an event Listener to each song
     Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
